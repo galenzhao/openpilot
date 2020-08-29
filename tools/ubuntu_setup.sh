bae3c2ea9fb40508e1e8cb059fb07f66bdb69565
Galen Zhao <me@zhenggangzhao.org>
2020-08-29 13:23:05 +0800

    Update ubuntu_setup.sh

--- tools/ubuntu_setup.sh
+++ tools/ubuntu_setup.sh
@@ -91,6 +91,10 @@ pyenv rehash
 
 # **** in python env ****
 
+# install pip proxy
+pip install pysocks
+export http_proxy=socks5://127.0.0.1:1080 https_proxy=socks5://127.0.0.1:1080
+
 # install pipenv
 pip install pipenv==2018.11.26
 
